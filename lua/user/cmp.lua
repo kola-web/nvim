437de981326c2f69f2eaeda56884e16579f6f653
kola <l15567540858@gmail.com>
2022-12-24 20:07:58 +0800

    cmp

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,67 +1,75 @@
-local cmp_status_ok, cmp = pcall(require, "cmp")
+local cmp_status_ok, cmp = pcall(require, 'cmp')
 if not cmp_status_ok then
-	return
+  return
 end
 
-local snip_status_ok, luasnip = pcall(require, "luasnip")
+local snip_status_ok, luasnip = pcall(require, 'luasnip')
 if not snip_status_ok then
-	return
+  return
 end
 
-local kind_status_ok, lspkind = pcall(require, "lspkind")
+local kind_status_ok, lspkind = pcall(require, 'lspkind')
 if not kind_status_ok then
-	return
+  return
 end
 
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets/" })
+require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load { paths = '~/.config/nvim/snippets/' }
 
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "path" },
-	}),
-	formatting = {
-		fields = { "kind", "abbr", "menu" },
-		format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
-	},
-})
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<tab>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources {
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+  formatting = {
+    fields = { 'kind', 'abbr', 'menu' },
+    format = lspkind.cmp_format { with_text = false, maxwidth = 50 },
+  },
+  window = {
+    documentation = false,
+    completion = {
+      border = 'rounded',
+      winhighlight = 'NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None',
+    },
+  },
+}
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ "/", "?" }, {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' },
+  }, {
+    { name = 'cmdline' },
+  }),
 })
+
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,18 +1,18 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+  PACKER_BOOTSTRAP = fn.system {
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path,
+  }
+  print 'Installing packer close and reopen Neovim...'
+  vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -24,110 +24,117 @@ end
 -- ]]
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
+local status_ok, packer = pcall(require, 'packer')
 if not status_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-	git = {
-		clone_timeout = 300, -- Timeout, in seconds, for git clones
-	},
-})
+packer.init {
+  display = {
+    open_fn = function()
+      return require('packer.util').float { border = 'rounded' }
+    end,
+  },
+  git = {
+    clone_timeout = 300, -- Timeout, in seconds, for git clones
+  },
+}
 
 -- Install your plugins here
 return packer.startup(function(use)
-	-- My plugins here
-	use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
-	use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
-	use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
-	use({ "numToStr/Comment.nvim" })
-	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
-	use({ "kyazdani42/nvim-web-devicons" })
-	use({ "kyazdani42/nvim-tree.lua" })
-	use({ "akinsho/bufferline.nvim" })
-	use({ "nvim-lualine/lualine.nvim" })
-	use({ "lewis6991/impatient.nvim" })
-	use({ "lukas-reineke/indent-blankline.nvim" })
-	use({ "goolord/alpha-nvim" })
-	use({ "folke/which-key.nvim" })
-	use({ "aserowy/tmux.nvim" })
+  -- My plugins here
+  use { 'wbthomason/packer.nvim' } -- Have packer manage itself
+  use { 'nvim-lua/plenary.nvim' } -- Useful lua functions used by lots of plugins
+  use { 'windwp/nvim-autopairs' } -- Autopairs, integrates with both cmp and treesitter
+  use { 'numToStr/Comment.nvim' }
+  use { 'JoosepAlviste/nvim-ts-context-commentstring' }
+  use { 'kyazdani42/nvim-web-devicons' }
+  use { 'kyazdani42/nvim-tree.lua' }
+  use { 'akinsho/bufferline.nvim' }
+  use { 'nvim-lualine/lualine.nvim' }
+  use { 'lewis6991/impatient.nvim' }
+  use { 'lukas-reineke/indent-blankline.nvim' }
+  use { 'goolord/alpha-nvim' }
+  use { 'folke/which-key.nvim' }
+  use { 'aserowy/tmux.nvim' }
 
-	use({ "kola-web/vim-indent-object" })
-	use({ "kana/vim-textobj-user" })
-	use({ "whatyouhide/vim-textobj-xmlattr" })
-	use({ "kana/vim-textobj-entire" })
-	use({ "wellle/targets.vim" })
-	use({ "tpope/vim-repeat" })
-	use({ "tpope/vim-surround" })
-	use({ "gbprod/substitute.nvim" })
-	use({ "johmsalas/text-case.nvim" })
-	use({ "andymass/vim-matchup" })
+  use { 'kola-web/vim-indent-object' }
+  use { 'kana/vim-textobj-user' }
+  use { 'whatyouhide/vim-textobj-xmlattr' }
+  use { 'kana/vim-textobj-entire' }
+  use { 'wellle/targets.vim' }
+  use { 'tpope/vim-repeat' }
+  use { 'tpope/vim-surround' }
+  use { 'gbprod/substitute.nvim' }
+  use { 'johmsalas/text-case.nvim' }
+  use { 'andymass/vim-matchup' }
 
-	-- Colorschemes
-	use({ "folke/tokyonight.nvim" })
-	use({ "catppuccin/nvim", as = "catppuccin" })
+  -- Colorschemes
+  use { 'folke/tokyonight.nvim' }
+  use { 'catppuccin/nvim', as = 'catppuccin' }
 
-	-- cmp plugins
-	use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
-	use({ "hrsh7th/cmp-buffer" }) -- buffer completions
-	use({ "hrsh7th/cmp-path" }) -- path completions
-	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
-	use({ "hrsh7th/cmp-nvim-lsp" })
-	use({ "hrsh7th/cmp-nvim-lua" })
+  -- cmp plugins
+  use { 'hrsh7th/nvim-cmp' } -- The completion plugin
+  use { 'hrsh7th/cmp-buffer' } -- buffer completions
+  use { 'hrsh7th/cmp-path' } -- path completions
+  use { 'saadparwaiz1/cmp_luasnip' } -- snippet completions
+  use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'hrsh7th/cmp-nvim-lua' }
 
-	-- snippets
-	use({ "L3MON4D3/LuaSnip" }) --snippet engine
-	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
-	use({ "ishan9299/nvim-solarized-lua" })
+  -- snippets
+  use { 'L3MON4D3/LuaSnip' } --snippet engine
+  use { 'rafamadriz/friendly-snippets' } -- a bunch of snippets to use
+  use { 'ishan9299/nvim-solarized-lua' }
 
-	-- LSP
-	-- use { "williamboman/nvim-lsp-installer",  } -- simple to use language server installer
-	use({ "onsails/lspkind-nvim" }) -- vscode-like pictograms
-	use({ "neovim/nvim-lspconfig" }) -- enable LSP
-	use({ "williamboman/mason.nvim" })
-	use({ "williamboman/mason-lspconfig.nvim" })
-	use({ "j-hui/fidget.nvim" })
-	use({ "jayp0521/mason-null-ls.nvim" })
-	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
-	use({ "RRethy/vim-illuminate" })
-	use({ "windwp/nvim-ts-autotag" })
-	use({ "b0o/schemastore.nvim" })
+  -- LSP
+  -- use { "williamboman/nvim-lsp-installer",  } -- simple to use language server installer
+  use { 'onsails/lspkind-nvim' } -- vscode-like pictograms
+  use { 'neovim/nvim-lspconfig' } -- enable LSP
+  use { 'williamboman/mason.nvim' }
+  use { 'williamboman/mason-lspconfig.nvim' }
+  use { 'j-hui/fidget.nvim' }
+  use { 'jayp0521/mason-null-ls.nvim' }
+  use { 'jose-elias-alvarez/null-ls.nvim' } -- for formatters and linters
+  use { 'RRethy/vim-illuminate' }
+  use { 'windwp/nvim-ts-autotag' }
+  use { 'b0o/schemastore.nvim' }
 
-	-- Telescope
-	use({ "nvim-telescope/telescope.nvim" })
-	use({ "nvim-telescope/telescope-file-browser.nvim" })
+  -- Telescope
+  use { 'nvim-telescope/telescope.nvim' }
+  use { 'nvim-telescope/telescope-file-browser.nvim' }
 
-	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-	})
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+  }
 
-	-- Git
-	use({ "lewis6991/gitsigns.nvim" })
+  -- Git
+  use { 'lewis6991/gitsigns.nvim' }
 
-	-- DAP
-	use({ "mfussenegger/nvim-dap" })
-	use({ "rcarriga/nvim-dap-ui" })
-	use({ "ravenxrz/DAPInstall.nvim" })
+  -- DAP
+  use { 'mfussenegger/nvim-dap' }
+  use { 'rcarriga/nvim-dap-ui' }
+  use { 'ravenxrz/DAPInstall.nvim' }
 
-	-- mark
-	use({ "chentoast/marks.nvim" })
+  -- mark
+  use { 'chentoast/marks.nvim' }
 
-	-- code run
-	use({ "CRAG666/code_runner.nvim", requires = "nvim-lua/plenary.nvim" })
+  -- code run
+  use { 'CRAG666/code_runner.nvim', requires = 'nvim-lua/plenary.nvim' }
 
-	-- markdown
+  -- markdown
+  use {
+    'iamcco/markdown-preview.nvim',
+    run = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  }
+  use { 'dhruvasagar/vim-table-mode' }
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
 end)
