4df6ef1663b7bc04bcab1a547ad58d546967cc47
kola <l15567540858@gmail.com>
2023-05-15 11:21:30 +0800

    update

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -29,50 +29,31 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   end,
 })
 
--- vim.api.nvim_create_autocmd({ "FileType" }, {
---   pattern = {
---     "Markdown",
---   },
---   callback = function()
---     vim.cmd [[
---       "autocmd Filetype markdown map <leader>w yiWi[<esc>Ea](<esc>pa)
---       inoremap <buffer> ,f <Esc>/<++><CR>:nohlsearch<CR>"_c4l
---       inoremap <buffer> <c-e> <Esc>/<++><CR>:nohlsearch<CR>"_c4l
---       inoremap <buffer> ,w <Esc>/ <++><CR>:nohlsearch<CR>"_c5l<CR>
---       inoremap <buffer> ,n ---<Enter><Enter>
---       inoremap <buffer> ,b **** <++><Esc>F*hi
---       inoremap <buffer> ,s ~~~~ <++><Esc>F~hi
---       inoremap <buffer> ,i ** <++><Esc>F*i
---       inoremap <buffer> ,d `` <++><Esc>F`i
---       inoremap <buffer> ,c ```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA
---       inoremap <buffer> ,m - [ ] 
---       inoremap <buffer> ,p ![](<++>) <++><Esc>F[a
---       inoremap <buffer> ,a [](<++>) <++><Esc>F[a
---       inoremap <buffer> ,1 #<Space><Enter><++><Esc>kA
---       inoremap <buffer> ,2 ##<Space><Enter><++><Esc>kA
---       inoremap <buffer> ,3 ###<Space><Enter><++><Esc>kA
---       inoremap <buffer> ,4 ####<Space><Enter><++><Esc>kA
---       inoremap <buffer> ,l --------<Enter>
---     ]]
---   end,
--- })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "Markdown",
+  },
+  callback = function()
+    vim.cmd [[
+      "autocmd Filetype markdown map <leader>w yiWi[<esc>Ea](<esc>pa)
+      inoremap <buffer> ,f <Esc>/<++><CR>:nohlsearch<CR>"_c4l
+      inoremap <buffer> <c-e> <Esc>/<++><CR>:nohlsearch<CR>"_c4l
+      inoremap <buffer> ,w <Esc>/ <++><CR>:nohlsearch<CR>"_c5l<CR>
+      inoremap <buffer> ,n ---<Enter><Enter>
+      inoremap <buffer> ,b **** <++><Esc>F*hi
+      inoremap <buffer> ,s ~~~~ <++><Esc>F~hi
+      inoremap <buffer> ,i ** <++><Esc>F*i
+      inoremap <buffer> ,d `` <++><Esc>F`i
+      inoremap <buffer> ,c ```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA
+      inoremap <buffer> ,m - [ ] 
+      inoremap <buffer> ,p ![](<++>) <++><Esc>F[a
+      inoremap <buffer> ,a [](<++>) <++><Esc>F[a
+      inoremap <buffer> ,1 #<Space><Enter><++><Esc>kA
+      inoremap <buffer> ,2 ##<Space><Enter><++><Esc>kA
+      inoremap <buffer> ,3 ###<Space><Enter><++><Esc>kA
+      inoremap <buffer> ,4 ####<Space><Enter><++><Esc>kA
+      inoremap <buffer> ,l --------<Enter>
+    ]]
+  end,
+})
 
--- vim.api.nvim_create_autocmd("ColorScheme", {
---   pattern = "*",
---   callback = function()
---     local hl_groups = {
---       "Normal",
---       "SignColumn",
---       "NormalNC",
---       "TelescopeBorder",
---       "NvimTreeNormal",
---       "NvimTreeNormalNC",
---       "EndOfBuffer",
---       "MsgArea",
---     }
---     for _, name in ipairs(hl_groups) do
---       vim.cmd(string.format("highlight %s ctermbg=none guibg=none", name))
---     end
---   end,
--- })
-vim.opt.fillchars = "eob: "
--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -71,7 +71,7 @@ cmp.setup {
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ "/", "?" }, {
-  mapping = cmp.mapping.preset.cmdline(),
+  -- mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "nvim_lsp_document_symbol" },
   }, {
@@ -81,7 +81,7 @@ cmp.setup.cmdline({ "/", "?" }, {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
+  -- mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
   }, { { name = "cmdline", option = { ignore_cmds = { "Man", "!" } } } }),
