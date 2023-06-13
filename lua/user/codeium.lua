35d800b4b68249f11d84ad212a20a1adf0498d21
kola <l15567540858@gmail.com>
2023-06-13 14:48:38 +0800

    update

--- lua/user/codeium.lua
+++ lua/user/codeium.lua
@@ -2,19 +2,21 @@ local M = {
   "Exafunction/codeium.vim",
   event = "InsertEnter",
   config = function()
+    vim.g.codeium_disable_bindings = 1
     vim.keymap.set("i", "<C-e>", function()
       return vim.fn["codeium#Accept"]()
     end, { expr = true })
-    vim.keymap.set("i", "<c-;>", function()
+    vim.keymap.set("i", "<C-;>", function()
       return vim.fn["codeium#CycleCompletions"](1)
     end, { expr = true })
-    vim.keymap.set("i", "<c-,>", function()
+    vim.keymap.set("i", "<C-,>", function()
       return vim.fn["codeium#CycleCompletions"](-1)
     end, { expr = true })
-    vim.keymap.set("i", "<c-x>", function()
+    vim.keymap.set("i", "<C-x>", function()
       return vim.fn["codeium#Clear"]()
     end, { expr = true })
   end,
 }
 
 return M
+
