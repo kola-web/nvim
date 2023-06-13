3ae4f323c6dbd34f23fa07fc7fdfbd80886384c8
kola <l15567540858@gmail.com>
2023-06-13 18:37:20 +0800

    update

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,12 +1,14 @@
 local M = {
   -- "folke/tokyonight.nvim",
   -- "projekt0n/github-nvim-theme",
-  "sainnhe/gruvbox-material",
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  -- "sainnhe/gruvbox-material",
+  "Mofiqul/dracula.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
-M.name = "gruvbox-material"
+-- M.name = "gruvbox-material"
+M.name = "dracula"
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
