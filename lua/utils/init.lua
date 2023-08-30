cd4ec5d01cb15cabd60a3c51c7fb774cd2b54fc9
kola <l15567540858@gmail.com>
2023-08-30 12:23:22 +0800

    update

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -43,6 +43,8 @@ end
 
 M.is_vue = function()
   local util = require "lspconfig.util"
+  local cwd = vim.fn.getcwd()
+  local project_root = util.find_node_modules_ancestor(cwd)
   local vue_path = util.path.join(project_root, "node_modules", "vue")
   local is_vue = vim.fn.isdirectory(vue_path) == 1
   return is_vue
