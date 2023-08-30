da1694d8c673367cd4c96e7385563224cfa51d6b
kola <l15567540858@gmail.com>
2023-08-30 12:01:27 +0800

    update

--- lua/settings/volar.lua
+++ lua/settings/volar.lua
@@ -3,7 +3,7 @@ local util = require "lspconfig.util"
 
 local function get_typescript_server_path(root_dir)
   local global_ts =
-    "/Users/lijialin/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib"
+  "/Users/lijialin/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib"
   local found_ts = ""
   local function check_dir(path)
     found_ts = util.path.join(path, "node_modules", "typescript", "lib")
@@ -20,27 +20,27 @@ end
 
 return {
   filetypes = fun.is_vue() and { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" }
-    or { "vue" },
+      or { "vue" },
   on_new_config = function(new_config, new_root_dir)
     if
-      new_config.init_options
-      and new_config.init_options.typescript
-      and new_config.init_options.typescript.tsdk == ""
+        new_config.init_options
+        and new_config.init_options.typescript
+        and new_config.init_options.typescript.tsdk == ""
     then
       new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
     end
   end,
-  documentFeatures = {
-    documentFormatting = {
-      defaultPrintWidth = 60,
-    },
-  },
-  settings = {
-    typescript = {
-      preferences = {
-        -- "relative" | "non-relative" | "auto" | "shortest"(not sure)
-        importModuleSpecifier = "non-relative",
-      },
-    },
-  },
+  -- documentFeatures = {
+  --   documentFormatting = {
+  --     defaultPrintWidth = 60,
+  --   },
+  -- },
+  -- settings = {
+  --   typescript = {
+  --     preferences = {
+  --       -- "relative" | "non-relative" | "auto" | "shortest"(not sure)
+  --       importModuleSpecifier = "non-relative",
+  --     },
+  --   },
+  -- },
 }
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -24,10 +24,27 @@ M.config = function()
 
   local actions = require "telescope.actions"
 
+  local previewers = require("telescope.previewers")
+
+  local new_maker = function(filepath, bufnr, opts)
+    opts = opts or {}
+
+    filepath = vim.fn.expand(filepath)
+    vim.loop.fs_stat(filepath, function(_, stat)
+      if not stat then return end
+      if stat.size > 100000 then
+        return
+      else
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      end
+    end)
+  end
+
   local opts = {
     defaults = {
       prompt_prefix = " ",
       selection_caret = " ",
+      buffer_previewer_maker = new_maker,
       file_ignore_patterns = {
         ".git",
         ".svn",
