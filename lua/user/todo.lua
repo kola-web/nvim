3f2f6d1d4a25e982c42136fd86085a806dadee14
kola <l15567540858@gmail.com>
2023-10-12 21:24:05 +0800

    update

--- lua/user/todo.lua
+++ lua/user/todo.lua
@@ -1,18 +1,27 @@
 local M = {
   'folke/todo-comments.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  config = true,
+  keys = {
+    {
+      ']t',
+      function()
+        require('todo-comments').jump_next()
+      end,
+      desc = 'Next todo comment',
+    },
+    {
+      '[t',
+      function()
+        require('todo-comments').jump_prev()
+      end,
+      desc = 'Previous todo comment',
+    },
+  },
 }
 
-M.config = function(_, opts)
-  vim.keymap.set('n', ']t', function()
-    require('todo-comments').jump_next()
-  end, { desc = 'Next todo comment' })
-
-  vim.keymap.set('n', '[t', function()
-    require('todo-comments').jump_prev()
-  end, { desc = 'Previous todo comment' })
-
-  require('noice').setup(opts)
-end
+-- M.config = function(_, opts)
+--   require('noice').setup(opts)
+-- end
 
 return M
