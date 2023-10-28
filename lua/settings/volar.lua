0fc3777ce88fd68c3d255ae7a5324bbf53ddd5bf
kola <l15567540858@gmail.com>
2023-10-28 11:51:09 +0800

    update

--- lua/settings/volar.lua
+++ lua/settings/volar.lua
@@ -1,27 +1,30 @@
+-- Language server `volar` does not support command `editor.action.showReferences`. This command may require a client extension.
 return {
   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
   -- filetypes = { 'typescript', 'javascript', 'vue' },
   init_options = {
     locale = 'zh-CN',
     languageFeatures = {
-      references = true,
-      implementation = true,
-      definition = true,
-      typeDefinition = true,
       callHierarchy = true,
-      hover = true,
-      rename = true,
-      renameFileRefactoring = true,
-      signatureHelp = true,
+      codeAction = true,
+      codeLens = true,
       completion = {
         defaultAttrNameCase = 'kebabCase',
-        defaultTagNameCase = 'kebabCase',
+        defaultTagNameCase = 'both',
       },
-      inlayHints = true,
+      definition = true,
       diagnostics = true,
-      codeLens = {
-        enabled = true,
-      },
+      documentHighlight = true,
+      documentLink = true,
+      hover = true,
+      implementation = true,
+      references = true,
+      rename = true,
+      renameFileRefactoring = true,
+      schemaRequestService = true,
+      semanticTokens = false,
+      signatureHelp = true,
+      typeDefinition = true,
     },
   },
   settings = {
