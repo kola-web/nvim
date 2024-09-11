5303dfe41ac2a7f5443fea98d3c768cfb33d3ce4
kola <l15567540858@gmail.com>
2024-09-11 14:26:30 +0800

    update

--- lua/user/neo-tree.lua
+++ lua/user/neo-tree.lua
@@ -117,24 +117,6 @@ local M = {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
       'MunifTanjim/nui.nvim',
-      {
-        's1n7ax/nvim-window-picker',
-        config = function()
-          require('window-picker').setup({
-            filter_rules = {
-              include_current_win = false,
-              autoselect_one = true,
-              -- filter using buffer options
-              bo = {
-                -- if the file type is one of following, the window will be ignored
-                filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
-                -- if the buffer type is one of following, the window will be ignored
-                buftype = { 'terminal', 'quickfix' },
-              },
-            },
-          })
-        end,
-      },
     },
     keys = {
       { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Explorer' },
