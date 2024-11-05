f2429d880e6a4c81d9a823e7b7dd9dd494412190
kola <l15567540858@gmail.com>
2024-11-05 14:11:10 +0800

    support wsl

---
+++ lua/system/wsl.lua
@@ -0,0 +1,29 @@
+vim.g.mini_component = function(state)
+  local fs_actions = require('neo-tree.sources.filesystem.lib.fs_actions')
+  local node = state.tree:get_node()
+  local currentPath = node.type == 'file' and node._parent_id or node.id
+  vim.fn.system({
+    'cp',
+    '-R',
+    '/Users/lijialin/.config/nvim/template/' .. 'wxmlComponent',
+    currentPath,
+  })
+  fs_actions.rename_node(currentPath .. '/wxmlComponent', function(_, path)
+    vim.cmd('edit ' .. path .. '/index.wxml')
+  end)
+end
+
+vim.g.mini_page = function(state)
+  local fs_actions = require('neo-tree.sources.filesystem.lib.fs_actions')
+  local node = state.tree:get_node()
+  local currentPath = node.type == 'file' and node._parent_id or node.id
+  vim.fn.system({
+    'cp',
+    '-R',
+    '/Users/lijialin/.config/nvim/template/' .. 'wxmlPage',
+    currentPath,
+  })
+  fs_actions.rename_node(currentPath .. '/wxmlPage', function(_, path)
+    vim.cmd('edit ' .. path .. '/index.wxml')
+  end)
+end
