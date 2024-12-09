61a2ed09cf43195bba8581a5a942890ebba898c3
kola-web <l15567540858@gmail.com>
2024-12-09 12:10:09 +0800

    fzf

--- lua/user/fzf.lua
+++ lua/user/fzf.lua
@@ -1,7 +1,7 @@
 local M = {
   'ibhagwan/fzf-lua',
   event = 'VeryLazy',
-  -- enabled = false,
+  enabled = false,
   opts = {
     'telescope',
     fzf_opts = {
@@ -21,9 +21,6 @@ local M = {
     file_ignore_patterns = { 'node_modules', '.git', 'dist', 'dist_pro', 'dist_uat', '.yarn', 'miniprogram_npm' },
     files = {
       -- previewer = 'bat',
-      -- find_opts = [[-type f -not -path '*/\.git/*' -not -path '*/.git/*' -not -path '*/node_modules/*' -not -path '*/miniprogram_npm/*' -not -path '*/.yarn/*' -not -path '*/static/*' -not -path '*/.svn/*' --not -path '*/statics/*' --not -path '*/dist/*' --not -path '*/dist_uat/*' --not -path '*/dist_pro/*']],
-      -- rg_opts = "--color=never --files --hidden --follow -g '!{.git,node_modules,miniprogram_npm,.yarn,static,.svn,statics,dist,dist_uat,dist_pro}'",
-      -- fd_opts = "--color=never --type f --hidden --follow --exclude '{.git,node_modules,miniprogram_npm,.yarn,static,.svn,statics,dist,dist_uat,dist_pro}'",
     },
     previewers = {
       builtin = {
