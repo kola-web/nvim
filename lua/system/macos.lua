4c8656b4713407fc432c527867f9920b59c9c6f2
kola-web <l15567540858@gmail.com>
2024-12-20 12:24:48 +0800

    up

--- lua/system/macos.lua
+++ lua/system/macos.lua
@@ -27,3 +27,27 @@ vim.g.mini_page = function(state)
     vim.cmd('edit ' .. path .. '/index.wxml')
   end)
 end
+
+vim.g.mini_file_mini_component = function(state)
+  local file = MiniFiles.get_fs_entry()
+  local currentPath = file.fs_type == 'directory' and file.path or string.gsub(file.path, '/' .. escape_pattern(file.name), '', 1)
+  vim.fn.system({
+    'cp',
+    '-R',
+    '/Users/lijialin/.config/nvim/template/' .. 'wxmlComponent',
+    currentPath,
+  })
+  MiniFiles.synchronize()
+end
+
+vim.g.mini_file_mini_page = function(state)
+  local file = MiniFiles.get_fs_entry()
+  local currentPath = file.fs_type == 'directory' and file.path or string.gsub(file.path, '/' .. escape_pattern(file.name), '', 1)
+  vim.fn.system({
+    'cp',
+    '-R',
+    '/Users/lijialin/.config/nvim/template/' .. 'wxmlPage',
+    currentPath,
+  })
+  MiniFiles.synchronize()
+end
