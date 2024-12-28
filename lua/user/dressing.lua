db4461741c5e78816b14d687cf14cb5a3e347289
kola-web <l15567540858@gmail.com>
2024-12-28 14:40:41 +0800

    main update

--- lua/user/dressing.lua
+++ lua/user/dressing.lua
@@ -7,11 +7,6 @@ local M = {
       require('lazy').load({ plugins = { 'dressing.nvim' } })
       return vim.ui.select(...)
     end
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.ui.input = function(...)
-      require('lazy').load({ plugins = { 'dressing.nvim' } })
-      return vim.ui.input(...)
-    end
   end,
   opts = {
     select = {
