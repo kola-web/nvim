e6939aa5f66b7ce821c231ce8210f84c80fcaa47
kola-web <l15567540858@gmail.com>
2025-02-11 15:56:08 +0800

    update

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,21 +1,19 @@
-local keymap = vim.keymap.set
--- Silent keymap option
-local opts = { silent = true, noremap = true, buffer = true }
-
-keymap("i", ",f", '<Esc>/<++><CR>:nohlsearch<CR>"_c4l', opts)
-keymap("i", "<c-e>", '<Esc>/<++><CR>:nohlsearch<CR>"_c4l', opts)
-keymap("i", ",w", '<++><CR>:nohlsearch<CR>"_c5l<CR>', opts)
-keymap("i", ",n", "---<Enter><Enter>", opts)
-keymap("i", ",b", "**** <++><Esc>F*hi", opts)
-keymap("i", ",s", "~~~~ <++><Esc>F~hi", opts)
-keymap("i", ",i", "** <++><Esc>F*i", opts)
-keymap("i", ",d", "`` <++><Esc>F`i", opts)
-keymap("i", ",c", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA", opts)
-keymap("i", ",m", "- [ ] ", opts)
-keymap("i", ",p", "![](<++>) <++><Esc>F[a", opts)
-keymap("i", ",a", "[](<++>) <++><Esc>F[a", opts)
-keymap("i", ",1", "#<Space><Enter><++><Esc>kA", opts)
-keymap("i", ",2", "##<Space><Enter><++><Esc>kA", opts)
-keymap("i", ",3", "###<Space><Enter><++><Esc>kA", opts)
-keymap("i", ",4", "####<Space><Enter><++><Esc>kA", opts)
-keymap("i", ",l", "--------<Enter>", opts)
+vim.cmd[[
+  inoremap <buffer> ,f <Esc>/<++><CR>:nohlsearch<CR>"_c4l
+  inoremap <buffer> <c-e> <Esc>/<++><CR>:nohlsearch<CR>"_c4l
+  inoremap <buffer> ,w <Esc>/ <++><CR>:nohlsearch<CR>"_c5l<CR>
+  inoremap <buffer> ,n ---<Enter><Enter>
+  inoremap <buffer> ,b **** <++><Esc>F*hi
+  inoremap <buffer> ,s ~~~~ <++><Esc>F~hi
+  inoremap <buffer> ,i ** <++><Esc>F*i
+  inoremap <buffer> ,d `` <++><Esc>F`i
+  inoremap <buffer> ,c ```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA
+  inoremap <buffer> ,m - [ ]
+  inoremap <buffer> ,p ![](<++>) <++><Esc>F[a
+  inoremap <buffer> ,a [](<++>) <++><Esc>F[a
+  inoremap <buffer> ,1 #<Space><Enter><++><Esc>kA
+  inoremap <buffer> ,2 ##<Space><Enter><++><Esc>kA
+  inoremap <buffer> ,3 ###<Space><Enter><++><Esc>kA
+  inoremap <buffer> ,4 ####<Space><Enter><++><Esc>kA
+  inoremap <buffer> ,l --------<Enter>
+]]
