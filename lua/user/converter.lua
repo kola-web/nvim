3d3906284a5479d647d6d50b7eda9eb6911a1436
kola-web <l15567540858@gmail.com>
2025-03-10 11:44:48 +0800

    change name

---
+++ lua/user/converter.lua
@@ -0,0 +1,15 @@
+local M = {
+  {
+    'NTBBloodbath/color-converter.nvim',
+    keys = {
+      {
+        '<A-->',
+        '<Plug>ColorConvertCycle',
+        desc = 'ColorConvertCycle',
+        silent = true,
+      },
+    },
+  },
+}
+
+return M
