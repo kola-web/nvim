1db5299e3a543e35c7cc8ab415ba9c424498c8ef
kola-web <l15567540858@gmail.com>
2025-04-17 12:26:30 +0800

    update

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -1,34 +1,4 @@
 local M = {
-  -- {
-  --   'romgrk/barbar.nvim',
-  --   event = 'VeryLazy',
-  --   init = function()
-  --     vim.g.barbar_auto_setup = false
-  --   end,
-  --   opts = {
-  --     animation = false,
-  --     sidebar_filetypes = {
-  --       ['neo-tree'] = {
-  --         text = 'neo-tree',
-  --         align = 'center',
-  --       },
-  --     },
-  --     icons = {
-  --       separator = { left = '│', right = '│' },
-  --       separator_at_end = false,
-  --     },
-  --     minimum_padding = 0,
-  --   },
-  --   keys = {
-  --     { '<S-tab>', '<cmd>BufferPrevious<cr>', desc = 'Prev buffer' },
-  --     { '<tab>', '<cmd>BufferNext<cr>', desc = 'Next buffer' },
-  --     { '(', '<cmd>BufferMovePrevious<cr>', desc = 'move prev' },
-  --     { ')', '<cmd>BufferMoveNext<cr>', desc = 'move move' },
-  --     { '<leader>br', '<cmd>BufferRestore<cr>', desc = 'Restore buffer' },
-  --     { '<leader>bp', '<Cmd>BufferPin<CR>', desc = 'Toggle Pin' },
-  --   },
-  -- },
-
   {
     'akinsho/bufferline.nvim',
     event = 'VeryLazy',
