04a422ac6aea042677e0c04cb1cef6b8bcd27a49
kola-web <l15567540858@gmail.com>
2025-06-27 09:34:31 +0800

    cmp update

--- lua/lsp/core/cmp.lua
+++ lua/lsp/core/cmp.lua
@@ -11,7 +11,6 @@ local M = {
     },
     opts = function()
       vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
-
       unpack = unpack or table.unpack
       local function has_words_before()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -34,20 +33,16 @@ local M = {
             require('luasnip').lsp_expand(args.body)
           end,
         },
-        mapping = {
-          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
-          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
-          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-          ['<C-c>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-          }),
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
           ['<Tab>'] = function(fallback)
             local opts = {
               select = true,
               behavior = cmp.ConfirmBehavior.Insert,
             }
-            if cmp.core.view:visible() then
+            if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
               require('utils.init').create_undo()
               if cmp.confirm(opts) then
                 return
@@ -58,27 +53,9 @@ local M = {
             end
             return fallback()
           end,
-          ['<C-n>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-p>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        },
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        }),
         sources = cmp.config.sources({
           { name = 'lazydev' },
           { name = 'nvim_lsp' },
