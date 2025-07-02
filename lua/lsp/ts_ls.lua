115d951aa1e706737cb8759d8d576a3350ce55aa
kola-web <l15567540858@gmail.com>
2025-07-02 18:27:51 +0800

    vtsls

--- lua/lsp/ts_ls.lua
+++ lua/lsp/ts_ls.lua
@@ -19,17 +19,3 @@ return {
   },
   settings = {},
 }
-
--- local fun = require('util')
---
--- return {
---   filetypes = fun.is_vue() and { 'null' } or { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
---   single_file_support = false,
---   init_options = {
---     hostInfo = 'neovim',
---     locale = 'zh-CN',
---     preferences = {
---       importModuleSpecifierPreference = 'non-relative',
---     },
---   },
--- }
--- lua/lsp/vtsls.lua
+++ lua/lsp/vtsls.lua
@@ -1,4 +1,6 @@
-local vue_typescript_plugin = require('mason-registry').get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server' .. '/node_modules/@vue/typescript-plugin'
+local vue_typescript_plugin = require('mason-registry').get_package('vue-language-server'):get_install_path()
+  .. '/node_modules/@vue/language-server'
+  .. '/node_modules/@vue/typescript-plugin'
 
 return {
   filetypes = {
@@ -8,31 +10,32 @@ return {
     'typescript',
     'typescriptreact',
     'typescript.tsx',
-    'vue',
   },
   settings = {
     complete_function_calls = true,
     vtsls = {
       enableMoveToFileCodeAction = true,
+      autoUseWorkspaceTsdk = true,
+      experimental = {
+        maxInlayHintLength = 30,
+        completion = {
+          enableServerSideFuzzyMatch = true,
+        },
+      },
       tsserver = {
         globalPlugins = {
           {
             name = '@vue/typescript-plugin',
             location = vue_typescript_plugin,
-            languages = { 'typescript', 'javascript', 'vue' },
-            enableForWorkspaceTypeScriptVersions = true,
+            languages = { 'vue' },
             configNamespace = 'typescript',
+            enableForWorkspaceTypeScriptVersions = true,
           },
         },
       },
     },
     typescript = {
       updateImportsOnFileMove = { enabled = 'always' },
-      experimental = {
-        completion = {
-          enableServerSideFuzzyMatch = true,
-        },
-      },
       suggest = {
         completeFunctionCalls = true,
       },
