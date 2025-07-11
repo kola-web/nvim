b0cd377f4ccc5ab4481b1154c85e062d5bb6725c
kola-web <l15567540858@gmail.com>
2025-07-11 15:04:06 +0800

    blink-alias-path

--- lua/lsp/blink.lua
+++ lua/lsp/blink.lua
@@ -70,27 +70,16 @@ local M = {
         },
       },
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+        default = { 'lsp', 'aliasPath', 'snippets', 'buffer', 'lazydev' },
         providers = {
           lazydev = {
             name = 'LazyDev',
             module = 'lazydev.integrations.blink',
             score_offset = 100, -- show at a higher priority than lsp
           },
-          path = {
-            module = 'blink.cmp.sources.path',
-            score_offset = 3,
-            fallbacks = { 'buffer' },
-            opts = {
-              trailing_slash = true,
-              label_trailing_slash = true,
-              get_cwd = function(context)
-                return vim.fn.expand(('#%d:p:h'):format(context.bufnr))
-              end,
-              show_hidden_files_by_default = false,
-              -- Treat `/path` as starting from the current working directory (cwd) instead of the root of your filesystem
-              ignore_root_slash = false,
-            },
+          aliasPath = {
+            name = 'aliasPath',
+            module = 'blink-alias-path',
           },
         },
       },
