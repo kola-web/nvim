2f51ba5e32481f8ebfea028cf15e11d16ab12ab5
kola-web <l15567540858@gmail.com>
2025-10-30 14:56:34 +0800

    Revert "index"
    This reverts commit 54ee01496f8a4b614adc5ecc6913b2432463b5d7.

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -80,7 +80,6 @@ local M = {
       })
 
       local icons = require('utils.icons')
-
       vim.diagnostic.config({
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
