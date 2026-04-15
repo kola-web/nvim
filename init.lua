419f29c15440b3ecf0290d2eb256a3df8096439c
kola-web <l15567540858@gmail.com>
2026-04-15 18:56:42 +0800

    update

--- init.lua
+++ init.lua
@@ -1,35 +1,30 @@
-require('options')
+local has = vim.fn.has
+local is_mac = has('macunix')
+local is_linux = has('unix')
+local is_win = has('win32')
+local is_wsl = has('wsl')
 
-if not vim.g.vscode then
-  local has = vim.fn.has
-  local is_mac = has('macunix')
-  local is_linux = has('unix')
-  local is_win = has('win32')
-  local is_wsl = has('wsl')
+if is_mac == 1 then
+  require('system.macos')
+end
+if is_linux == 1 then
+  require('system.linux')
+end
+if is_win == 1 then
+  require('system.windows')
+end
 
-  if is_mac == 1 then
-    require('system.macos')
-  end
-  if is_linux == 1 then
-    require('system.linux')
-  end
-  if is_win == 1 then
-    require('system.windows')
-  end
-
-  if is_wsl == 1 then
-    require('system.wsl')
-  end
-
-  require('keymaps')
-  require('autocommands')
+if is_wsl == 1 then
+  require('system.wsl')
 end
 
 if vim.g.neovide then
   require('system.neovide')
 end
 
+require('options')
+require('keymaps')
+require('autocommands')
 require('pack')
-
 require('user.init').setup()
 require('lsp.init').setup()
